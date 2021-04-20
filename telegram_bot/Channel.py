a4af2d9efb5ba33b8b15cfc60330b8fb026d601a
Alexey Davletshin <naylrush@gmail.com>
2021-04-20 19:21:25 +0300

    Update Channel class

--- telegram_bot/Channel.py
+++ telegram_bot/Channel.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timezone
 
 
+def now():
+    return datetime.utcnow().replace(tzinfo=timezone.utc)
+
+
 class Channel:
     def __init__(self, id: int, last_update: datetime = None):
         self.id = id
-        self.last_update = last_update
+        self.last_update = last_update if last_update else now()
 
     def update(self):
-        dttm = self.last_update
-        self.last_update = datetime.utcnow().replace(tzinfo=timezone.utc)
+        dttm, self.last_update = self.last_update, now()
         return dttm
